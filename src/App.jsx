74b348aef9737a737d15d81db815c7baf790f3f7
troy flavell <troyflavell@troys-MacBook-Pro.local>
2024-04-29 14:10:27 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+  
+
+  return (
+<main>
+  <div className="div" id="game-container">
+    Players 
+    <ol id="players">
+      <li>
+        <span className="player-name">Player 1</span>
+        <span className="player-symbol">X</span>
+      </li>
+      <li><span>Player 2</span></li>
+    </ol>
+    Game Board
+  </div>
+
+  Log
+</main>  )
+}
+
+export default App
